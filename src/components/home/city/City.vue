6b71eaf293d30629058a956cfcde8f82d0232b39
msppyu1 <873634131@qq.com>
2019-04-19 22:24:16 +0800

    'msg'

---
+++ src/components/home/city/City.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        hhah
+    </div>
+</template>
+
+<script>
+export default {
+    name:'city'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home/Home'
-
+import City from '@/components/home/City'
 
 Vue.use(Router)
 
@@ -12,6 +12,10 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    
+    {
+      path: '/city',
+      name: 'city',
+      component: City
+    }
   ]
 })
